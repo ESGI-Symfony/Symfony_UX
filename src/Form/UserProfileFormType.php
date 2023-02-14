8c088d0f6ea994d85c027d7f7e0b7a319c445e0f
Laurie MORIN <laurie20010701@gmail.com>
2023-02-14 19:17:53 +0100

    edit account page

---
+++ src/Form/UserProfileFormType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserProfileFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'placeholder' => 'your_email',
+                ]
+            ])
+            ->add('nickname', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'your_nickname',
+                ],
+            ])
+            ->add('firstname', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'your_name',
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'your_last_name',
+                ],
+            ])
+            ->add('phone', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'your_galactic_phone',
+                ],
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
