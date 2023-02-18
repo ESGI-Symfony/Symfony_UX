17d8b74455ce260986fec15e33789eace0faef10
Laurie MORIN <laurie20010701@gmail.com>
2023-02-18 11:49:24 +0100

    remove sensio bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -17,5 +17,4 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     whatwedo\TwigBootstrapIcons\whatwedoTwigBootstrapIconsBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
